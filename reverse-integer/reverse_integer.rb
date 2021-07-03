b1e9184bacb6b5c90cfb988c69ed65fd4b829f55
Lydia Scadding <lydiascadding@gmail.com>
2021-07-03 21:04:36 +0100

    Completed with a more efficient solution

--- reverse-integer/reverse_integer.rb
+++ reverse-integer/reverse_integer.rb
@@ -1,3 +1,17 @@
 def reverse(x)
-  x.to_s.gsub(/\d+/, &:reverse).to_i
+  reversed = 0
+  original = x.abs
+  while original > 0
+    reversed = reversed * 10 + original % 10
+    original /= 10
+  end
+  reversed = x.negative? ? reversed * -1 : reversed
+  reversed < 2**31 && reversed > -2**31 ? reversed : 0
 end
+
+# Alternative solution, fractionally less efficient:
+
+# def reverse(x)
+#   reversed = x.to_s.gsub(/\d+/, &:reverse).to_i
+#   reversed < 2**31 && reversed > -2**31 ? reversed : 0
+# end
--- reverse-integer/spec/reverse_integer_spec.rb
+++ reverse-integer/spec/reverse_integer_spec.rb
@@ -29,9 +29,16 @@ describe "#reverse" do
     expect(actual).to eq(expected)
   end
 
-  it "returns the correct answer for a longer number" do
-    actual = reverse(-12948734059834)
-    expected = -43895043784921
+  it "returns 0 if the reversed value is above 2**31" do
+    actual = reverse(8123643123)
+    expected = -0
+
+    expect(actual).to eq(expected)
+  end
+
+  it "returns 0 if the reversed value is below -2**31" do
+    actual = reverse (-8123643123)
+    expected = 0
 
     expect(actual).to eq(expected)
   end
