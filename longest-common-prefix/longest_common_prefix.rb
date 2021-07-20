a012e26bc9d5611a663433ee83ae0e0bdb9093fd
Lydia Scadding <lydiascadding@gmail.com>
2021-07-20 09:15:26 +0100

    Alternative solution using while loop

--- longest-common-prefix/longest_common_prefix.rb
+++ longest-common-prefix/longest_common_prefix.rb
@@ -3,12 +3,17 @@ def longest_common_prefix(strs)
   return strs.first if strs.length == 1
 
   longest = ""
-  strs[0].chars.each_with_index do |char, idx|
-    if strs.all? { |str| str[idx] == char }
-      longest << char
-    else
-      break
-    end
+  # strs[0].chars.each_with_index do |char, idx|
+  #   if strs.all? { |str| str[idx] == char }
+  #     longest << char
+  #   else
+  #     break
+  #   end
+  # end
+  idx = 0
+  while strs.all? { |str| str[idx] == strs.first[idx] }
+    longest << strs.first[idx]
+    idx += 1
   end
   longest
 end
